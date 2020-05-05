f54fd77f836fad09ebdf586e6ec8f12e8ab3dcc1
mzgtrifork <mzg@trifork.com>
2020-05-05 21:59:42 +0200

    build docker image from local build and update run script

---
+++ run-docker-image.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# docker run \
+#        --name=hapi-fhir \
+#        --rm \
+#        -p 8080:8080 \
+#        -e HAPI_IMPLEMENTATION_GUIDE_URL=https://build.fhir.org/ig/hl7dk/dk-medcom/package.tgz \
+#        my-hapi-fhir-jpaserver-starter
+
+docker run \
+       --name=hapi-fhir \
+       --rm \
+       -p 8080:8080 \
+       -e HAPI_FHIR_VERSION=DSTU3 \
+       -e HAPI_IMPLEMENTATION_GUIDE_URL=https://docs.ehealth.sundhed.dk/latest/ig/package.tgz \
+       -e HAPI_TREAT_REFERENCES_AS_LOGICAL=http://ehealth.sundhed.dk/vs/*,http://ehealth.sundhed.dk/cs/* \
+       my-hapi-fhir-jpaserver-starter
